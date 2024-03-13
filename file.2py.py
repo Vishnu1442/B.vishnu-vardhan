9ebde1a8f161fda7a25bd484358ae6f14bdad051
Vishnu1442 <162986057+Vishnu1442@users.noreply.github.com>
2024-03-13 09:18:57 +0530

    Add files via upload

---
+++ file.2py.py
@@ -0,0 +1,207 @@
+
+
+
+
+
+#print(hash(f1)) # error --> list non-constant or mutable datatype
+
+#! ----> operators
+# operators are symbols which is used to perform various operations
+# between 2 or more operands
+
+# Arithmatic
+# Assignment
+# Logical
+# Relational or comparsion
+# Bitwise
+# Identity
+# Membership
+
+# todo ----> Arithmatic --> +, -,*,/,%,//,**
+#Eg:1
+a=8
+b=6
+c=9
+# print(a+b+c)
+
+# input() --> used to get the routime input
+# eval() ---> used toget the runtime values of all datatype
+
+n1 = int(input("enter the value: "))
+# print(type(n1))
+
+# a = 4
+# b = 2
+# print (a/b) # is used to get the quotient value
+# print (a%b) # is used to get the remainder value
+
+# ! // ----> floor devision
+
+# a = 765433
+# b = 19
+# print(a/b) # -> the output will be be in integer & the output
+# based on floor value
+
+# ! ** --> used for a power of a number
+print(2**4) # --> 16
+
+# ! assigment ---> +-=, -=, /=, //=, **=, &=, |=, %=
+# a=8
+# b+=2
+# print(a)
+
+# a=30
+# b-=5
+# print(a)
+
+# ! comparision --> &, |, ^, ~, <<, >>
+# a=9
+# b=5
+print(a==b)
+
+# ! bitwise operator --> &, |, ^, ~, <<,>>
+# a = 7
+# b = 4
+print(a&b)
+
+# 2^4 2^3 2^1 2^0
+# 8    4   2   1
+
+# 0    1   1   1   # --> 7
+# 0    1   0   0   # --> 6 &
+# -------------------
+# 0    1   1
+
+# XOR
+# A B A^B
+# 0 0 0
+
+# 0 1  0
+#1  0  0
+#1  1  1
+
+# ~ --->
+# a = 9876
+# print(~a)
+
+# a = 9
+
+
+# 128 64 32 16 8 4 2 1
+#  0   0  0  0 1 0 0 1 # -->  9
+
+#  1   1  1  1 1 1 1 0 # --> -10
+
+#  0  0  0  0  1  0 1 0 --> 10
+
+# 1 1 1 1 0 1 1 0 --> -10
+
+# 256 128 64 32 16 8 4 2 1
+# 107
+
+ # <<, >>
+ # print(5>>1)
+ # 16>4
+
+ # ! Logical --> used to compare the  expressions
+ # and, or , not
+ # a = 6
+  # print(a>3 and a<10)
+ # print(a>7 and a<30)
+ # print(not(a>8 and a<10))
+
+ #! identity operator
+ # ? are stored
+
+ 
+ # a = 8
+ # b = 8
+ # print(a is b)
+ # print(a==b)
+ 
+ # a = (1, 2, 3)
+ # b = (1, 2, 3)
+ # print(a is b)
+
+ # ! membership operator
+ # in, not in
+ # 11 = [7, 8, 9, 0, 6, 5]
+
+
+ # if, elif, else
+
+ # python syntax
+ # if condition:
+ #    statement
+ #    statement
+ #    ststement
+ # statement
+
+ # eg:1
+ # a = 6
+ # if a:
+ #     print("hello")
+0
+
+
+
+00
+ # Eg:2
+ # a = 6
+ #    print("hey")
+ # else:
+ #  print("no")
+
+ # Eg:3
+ # if 7>8:
+    #print ("hello")
+
+  # print("yes")
+
+  # ehg:4
+  # a = 0
+  # a = None
+  # a=False
+  # a=""
+  # if a:
+  #    Print("yes")
+  # else:
+  #    print("no")
+
+  # sum:1
+  # a number is even or odd
+  # n = int(input("enter the number: "))
+  # if n%2==0:
+  #   print(n,"is even")
+  #else:
+  # print(n, "is odd)
+
+  # sum:2
+  # name, age, nationality
+  # 18 above, Iandian
+
+  #  name = str(input("enter the name"))
+  #  age  = int(input("enter the age"))
+  #  nationality = input("enter the nation:")
+
+ # if age>=18 and nationally=="Iandian":
+    #print("eligible for vote")
+    #else:
+          #print("not eligible")
+   
+  
+  
+
+
+
+
+  
+  
+ 
+ 
+
+
+
+ 
+
+
